85f37476a6139e25b990bb308341452e1deee982
Linda Foo <linda.foo.dev@gmail.com>
2018-03-12 14:09:05 -0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+
+//export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
