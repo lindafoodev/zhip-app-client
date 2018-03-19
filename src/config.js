17ed2d7139a57d778de5e6c463c4607f2c8ede39
Linda Foo <linda.foo.dev@gmail.com>
2018-03-19 15:30:42 -0700

    change config for url link based on comments from joe

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+REACT_APP: export const CLIENT_ORIGIN = process.env.REACT_APP_CLIENT_ORIGIN || 'http://localhost:3000';
