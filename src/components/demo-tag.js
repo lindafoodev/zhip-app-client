e0c91e2958d8a247dafacbc98353b306c068698a
Linda Foo <33673643+lindafoodev@users.noreply.github.com>
2018-07-13 12:39:53 -0700

    return to previous state

--- src/components/demo-tag.js
+++ src/components/demo-tag.js
@@ -3,10 +3,7 @@ import {connect} from 'react-redux';
 
 export function DemoTag(props) {
     return (
-         <section>
-            <div className="tag">For purpose of Demo only, Zhip ID for Demo Account:<br/> 5ada279505f6051254052891</div>
-            <div className="tag">For purpose of Demo only, Zhip ID to claim an IOU created by Demo Account:<br/> 5b48f934693a4100148c263d</div>
-        </section>
+            <div className="tag">For purpose of Demo only, Zhip ID for account:<br/> 5ada279505f6051254052891</div>
     );
 }
 
